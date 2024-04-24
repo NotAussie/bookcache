47d9e835c3eb32bbe13725d25b88ca23384863e4
NotAussie <NotAussie@duck.com>
2024-04-24 16:36:53 +0930

    [🆙] Alpha stage update!

--- bookcache/errors/__init__.py
+++ bookcache/errors/__init__.py
@@ -22,37 +22,5 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-# Import types
-from pydantic import BaseModel
-from datetime import datetime
-import typing
-
-
-# Create type
-class CachedItem(BaseModel):
-    """
-    Example usage of `CachedItem`.
-
-    ----
-
-    .. code-block:: python3
-
-    # Import modules
-    from datetime import datetime, timedelta
-
-    # Create the time object so it cannot drift
-    time = datetime.now()
-
-    # Create a new CachedItem
-    CachedItem(
-        createdAt = time,
-        expireAt = time + timedelta(minutes=15),
-        librarianIteration = 0,
-        value = "Hello world!!",
-    )
-    """
-
-    createdAt: datetime
-    expireAt: datetime
-    librarianIteration: int
-    value: typing.Any
+from .base import Error
+from .notFound import NotFound
